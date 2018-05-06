6d6373fc2be160667241c6979b0e480817e9c99f
menglong <602419085@qq.com>
2018-05-06 13:19:49 +0800

    程序设计

---
+++ readme.rd
@@ -0,0 +1,24 @@
+
+
+
+Shiro + SpringMvc + Mybatis + Redis 的Demo
+
+使用过程：
+
+1.创建数据库。
+创建语句		   ：tables.sql
+
+2.插入初始化数据
+插入初始化数据：init.data.sql
+
+
+3.运行。
+
+
+管理员帐号：admin
+密码：sojson
+
+
+
+账户test1 、 test2 、 test3
+密码：123456
---
+++ src/main/java/com/sojson/permission/controller/RoleController.java
@@ -0,0 +1,105 @@
+package com.sojson.permission.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.sojson.common.controller.BaseController;
+import com.sojson.common.model.URole;
+import com.sojson.common.utils.LoggerUtils;
+import com.sojson.core.mybatis.page.Pagination;
+import com.sojson.permission.service.RoleService;
+import com.sojson.user.manager.UserManager;
+
+/**
+ * <p>
+ * <p>
+ * 用户属性管理
+ * <p>
+ * <p>
+ */
+@Controller
+@Scope(value = "prototype")
+@RequestMapping("role")
+public class RoleController extends BaseController {
+    @Autowired
+    RoleService roleService;
+
+    /**
+     * 属性列表
+     *
+     * @return
+     */
+    @RequestMapping(value = "index")
+    public ModelAndView index(String findContent, ModelMap modelMap) {
+        modelMap.put("findContent", findContent);
+        Pagination<URole> role = roleService.findPage(modelMap, pageNo, pageSize);
+        return new ModelAndView("role/index", "page", role);
+    }
+
+    /**
+     * 属性添加
+     *
+     * @param role
+     * @return
+     */
+    @RequestMapping(value = "addRole", method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String, Object> addRole(URole role) {
+        try {
+            int count = roleService.insertSelective(role);
+            resultMap.put("status", 200);
+            resultMap.put("successCount", count);
+        } catch (Exception e) {
+            resultMap.put("status", 500);
+            resultMap.put("message", "添加失败，请刷新后再试！");
+            LoggerUtils.fmtError(getClass(), e, "添加属性报错。source[%s]", role.toString());
+        }
+        return resultMap;
+    }
+
+    /**
+     * 删除属性，根据ID，但是删除属性的时候，需要查询是否有赋予给用户，如果有用户在使用，那么就不能删除。
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "deleteRoleById", method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String, Object> deleteRoleById(String ids) {
+        return roleService.deleteRoleById(ids);
+    }
+
+    /**
+     * 我的权限页面
+     *
+     * @return
+     */
+    @RequestMapping(value = "mypermission", method = RequestMethod.GET)
+    public ModelAndView mypermission() {
+        return new ModelAndView("permission/mypermission");
+    }
+
+    /**
+     * 我的权限 bootstrap tree data
+     *
+     * @return
+     */
+    @RequestMapping(value = "getPermissionTree", method = RequestMethod.POST)
+    @ResponseBody
+    public List<Map<String, Object>> getPermissionTree() {
+        //查询我所有的属性 ---> 权限
+        List<URole> roles = roleService.findNowAllPermission();
+        //把查询出来的roles 转换成bootstarp 的 tree数据
+        List<Map<String, Object>> data = UserManager.toTreeData(roles);
+        return data;
+    }
+}
